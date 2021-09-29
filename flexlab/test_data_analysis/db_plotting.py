b6478002bf10fef8f5278dea80ec83f7e26919dc
Rongxin Yin <rongxinyin@gmail.com>
2021-09-29 11:57:04 -0700

    add data analysis scripts

---
+++ flexlab/test_data_analysis/db_plotting.py
@@ -0,0 +1,226 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# GENERAL PACKAGE IMPORT
+# ----------------------
+from db_interface import DB_Interface
+import requests
+import numpy as np
+import json
+import pandas as pd
+from pandas import DataFrame
+from io import StringIO
+
+import datetime as dt
+import datetime
+import matplotlib
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from matplotlib.dates import date2num
+import seaborn
+import timeit
+import json
+import pickle
+
+import matplotlib.pyplot as plt
+import datetime
+
+
+# In[11]:
+
+
+def energy_plot(data, list_1, list_2, title, y_label):
+    fig, (ax) = plt.subplots(nrows = 1, ncols = 1, constrained_layout=True, facecolor='w', edgecolor='k',figsize = (18,9))
+    fig.subplots_adjust(hspace = .2, wspace=.15)
+
+    inp_day = data['date'].unique() # capture unique dates from the date column
+    inp_day = inp_day.tolist() #convert to a list
+
+    # create labels for each date of the test
+    labels = []
+    labels1 = []
+    for i in range(0,len(inp_day)):
+        labels.append(inp_day[i].strftime('%m/%d'))
+
+    #for i in range(0,len(inp_day)):
+    #    labels1.append(inp_day[i].strftime('%m/%d'))
+    
+    x = np.arange(len(labels))
+    width = .2
+    
+    ax.bar(x-width*.5, list_1, width = width, label='Baseline Control', color = 'grey')
+    ax.bar(x+width*.5, list_2, width = width, label='Test Control', color = 'indianred')
+    #ax.bar(x+width*.5, en_g36, width = width, label='G36 Control', color = 'rosybrown')
+    #ax.bar(x+width*1.5, en_g36_dr, width = width, label='G36 Control + DR', color = 'indianred')
+    ax.set_xticks(range(len(labels)))
+    ax.set_xticklabels(labels, fontsize = 12)
+    ax.set_title(title, fontsize = 18)
+    ax.set_ylabel(y_label, fontsize = 12)
+    ax.legend(fontsize = 9, loc = 2)
+
+
+# In[1]:
+
+
+def shed_demand_plot(data, list_1, list_2, title, y_label):
+    fig, (ax) = plt.subplots(nrows = 1, ncols = 1, constrained_layout=True, facecolor='w', edgecolor='k',figsize = (18,9))
+    fig.subplots_adjust(hspace = .2, wspace=.15)
+
+    inp_day = data['date'].unique() # capture unique dates from the date column
+    inp_day = inp_day.tolist() #convert to a list
+    #print(inp_day)
+    # create labels for each date of the test
+    labels = []
+    labels1 = []
+    for i in range(0,len(inp_day)):
+        labels.append(inp_day[i].strftime('%m/%d'))
+
+    for i in range(0,len(inp_day)-1):
+        labels1.append(inp_day[i].strftime('%m/%d'))
+
+    x = np.arange(len(labels))
+    print(labels)
+    print(list_2)
+    width = .2
+
+    ax.bar(x-width*.5, list_1, width = width, label='Baseline Control', color = 'dodgerblue')
+    ax.bar(x+width*.5, list_2, width = width, label='Test Control', color = 'mediumseagreen')
+
+    ax.set_xticks(range(len(labels)))
+    ax.set_xticklabels(labels[0:len(labels)], fontsize = 12)
+    ax.set_title(title, fontsize = 18)
+    ax.set_ylabel(y_label, fontsize = 12)
+    ax.legend(fontsize = 9, loc = 2)
+
+
+# In[ ]:
+
+
+def precool_demand_plot(data, list_1, list_2, title, y_label):
+    fig, (ax) = plt.subplots(nrows = 1, ncols = 1, constrained_layout=True, facecolor='w', edgecolor='k',figsize = (18,9))
+    fig.subplots_adjust(hspace = .2, wspace=.15)
+
+    inp_day = data['date'].unique() # capture unique dates from the date column
+    inp_day = inp_day.tolist() #convert to a list
+    #print(inp_day)
+    # create labels for each date of the test
+    labels = []
+    labels1 = []
+    for i in range(0,len(inp_day)):
+        labels.append(inp_day[i].strftime('%m/%d'))
+
+    for i in range(0,len(inp_day)-1):
+        labels1.append(inp_day[i].strftime('%m/%d'))
+
+    x = np.arange(len(labels))
+    print(labels)
+    print(list_2)
+    width = .2
+
+    ax.bar(x-width*.5, list_1, width = width, label='Baseline Control', color = 'skyblue')
+    ax.bar(x+width*.5, list_2, width = width, label='Test Control', color = 'lightgreen')
+
+    ax.set_xticks(range(len(labels)))
+    ax.set_xticklabels(labels[0:len(labels)], fontsize = 12)
+    ax.set_title(title, fontsize = 18)
+    ax.set_ylabel(y_label, fontsize = 12)
+    ax.legend(fontsize = 9, loc = 2)
+
+
+# In[1]:
+
+
+import matplotlib.dates as mdates
+
+def thermal_comfort_plot(data_1, inp_1, inp_2, inp_3, inp_4, inp_5, inp_6, scenario_dict, metric, ylabel, title_dict):
+    fig, (ax, ax1, ax2) = plt.subplots(nrows = 3, ncols = 1, constrained_layout=True, facecolor='w', edgecolor='k',
+                                       figsize = (18,9))
+    fig.subplots_adjust(hspace = .2, wspace=.15)
+
+    inp_day = data_1['date'].unique() # capture unique dates from the date column
+    inp_day = inp_day.tolist() #convert to a list
+
+    # create labels for each date of the test
+    labels = []
+    labels1 = []
+    for i in range(0,len(inp_day)):
+        labels.append(inp_day[i].strftime('%m/%d'))
+
+    print(labels)
+
+    x = np.array(inp_day)
+    
+    print(x)
+    print(inp_1[metric][0:len(labels)+1])
+    
+    shift = datetime.timedelta(hours = 5)
+    width = 0.2
+    
+    #ax.xaxis_date()
+    ax.get_xaxis().set_visible(False)
+    ax02 = ax.twinx()
+    ax02.plot(data_1['1B-OAT-1'])
+    #ax.set_xticks(range(len(labels)))
+    ax.bar(x-shift, inp_1[metric][0:len(labels)+1], width = width, label='Baseline Control', color = 'darkgrey')
+    ax.bar(x+shift, inp_2[metric][0:len(labels)+1], width = width, label='Test Control', color = 'dimgrey')
+    
+    ax12 = ax1.twinx()
+    ax12.plot(data_1['1B-OAT-1'])
+    ax1.bar(x-shift, inp_3[metric][0:len(labels)+1], width = width, label='Baseline Control', color = 'mediumseagreen')
+    ax1.bar(x+shift, inp_4[metric][0:len(labels)+1], width = width, label='Test Control', color = 'olivedrab')
+    
+    
+    ax22 = ax2.twinx()
+    ax22.plot(data_1['1B-OAT-1'])
+    ax2.bar(x-shift, inp_5[metric][0:len(labels)+1], width = width, label='Baseline Control', color = 'indianred')
+    ax2.bar(x+shift, inp_6[metric][0:len(labels)+1], width = width, label='Test Control', color = 'crimson')
+    
+        
+    fig.suptitle(title_dict, fontsize = 18)
+    
+    ax.set_title('North zone', fontsize = 16)
+    ax.set_ylabel(ylabel, fontsize = 9)
+    ax.legend(fontsize = 9, loc = 2)
+    if metric == 0:
+        ax.set_ylim(0,2.5)
+    else:
+        ax.set_ylim(0,0.5)
+
+    
+    ax1.legend(fontsize = 9, loc = 2)
+    ax1.set_title('Core zone', fontsize = 16)
+    ax1.set_ylabel(ylabel, fontsize = 9)
+    if metric == 0:
+        ax1.set_ylim(0,2.5)
+    else:
+        ax1.set_ylim(0,0.5)
+        
+    ax1.get_xaxis().set_visible(False)
+
+    ax2.legend(fontsize = 9, loc = 2)
+    ax2.set_title('South zone', fontsize = 16)
+    ax2.set_ylabel(ylabel, fontsize = 9)
+    #ax2.set_xticks(range(len(labels)))
+    if metric == 0:
+        ax2.set_ylim(0,2.5)
+    else:
+        ax2.set_ylim(0,0.5)
+    
+    #ax2.xaxis_date()
+    ax2.set_xticklabels(labels, fontsize = 12)
+
+    ax2.xaxis.set_major_locator(mdates.DayLocator(interval=1))  
+    ax2.xaxis.set_major_formatter(mdates.DateFormatter('%m/%d')) 
+    for label in ax2.get_xticklabels():
+        label.set_rotation(40)
+
+
+# In[ ]:
+
+
+
+
